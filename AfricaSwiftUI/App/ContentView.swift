21b773f210b4083421ee71183ad8222bdd40fc75
Sergey <bindasov.sergey@gmail.com>
2023-05-12 15:26:02 +0200

    navigation

--- AfricaSwiftUI/App/ContentView.swift
+++ AfricaSwiftUI/App/ContentView.swift
@@ -20,8 +20,10 @@ struct ContentView: View {
                     .frame(height: 300)
                     .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
                 ForEach(animals) { animal in
-                    AnimalSwiftUIView(animal: animal)
-                }
+                    NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                        AnimalSwiftUIView(animal: animal)
+                    } //: LINK
+                } //: LOOP
             } //: LIST
             .navigationBarTitle("Africa", displayMode: .large)
         } //: NAVIGATION
