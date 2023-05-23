b6d7b8858b6324c45ec47e4e2b86a02a0c605a6a
Sergey <bindasov.sergey@gmail.com>
2023-05-23 20:58:50 +0200

    map + 3 different pin style

--- AfricaSwiftUI/App/MapView.swift
+++ AfricaSwiftUI/App/MapView.swift
@@ -6,10 +6,37 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    
+    @State private var region: MKCoordinateRegion = {
+        var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+        return mapRegion
+    }()
+    
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+    
     var body: some View {
-        Text("Map")
+        // MARK: - No1 BASIC MAP
+        // Map(coordinateRegion: $region)
+        // MARK: - No1 ADVANCED MAP
+        Map(coordinateRegion: $region, annotationItems: locations, annotationContent: {
+            item in
+            // A - OLD STYLE
+            //MapPin(coordinate: item.location, tint: .accentColor)
+            // B - MARKER
+            //MapMarker(coordinate: item.location, tint: .accentColor)
+            // C - CUSTOM ANNOTATION
+            MapAnnotation(coordinate: item.location) {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32, alignment: .center)
+            } // END OF ANNOTATION
+        })
     }
 }
 
---
+++ AfricaSwiftUI/Model/LocationModel.swift
@@ -0,0 +1,21 @@
+//
+//  LocationModel.swift
+//  AfricaSwiftUI
+//
+//  Created by Sergey on 23.05.2023.
+//
+
+import Foundation
+import MapKit
+
+struct NationalParkLocation: Codable, Identifiable {
+    let id: String
+    let name: String
+    let image: String
+    let latitude: Double
+    let longitude: Double
+    
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
